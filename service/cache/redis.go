8d892562f1fb37e95f6fd269ab42ac25c8246779
ivailo.angelov <ivailo>
2021-08-20 18:19:22 +0300

    taking redis from env

--- service/cache/redis.go
+++ service/cache/redis.go
@@ -1,12 +1,24 @@
 package cache
 
 import (
+	"os"
+
 	"github.com/go-redis/redis/v8"
 )
 
 func OpenRedisConnection() *redis.Client {
+	host := "redis"
+	if hostEnv := os.Getenv("REDIS_HOST"); hostEnv != "" {
+		host = hostEnv
+	}
+
+	port := "6379"
+	if portEnv := os.Getenv("REDIS_PORT"); portEnv != "" {
+		port = portEnv
+	}
+
 	rdb := redis.NewClient(&redis.Options{
-		Addr:     "redis:6379",
+		Addr:     host + ":" + port,
 		Password: "", // no password set (add password)
 		DB:       0,  // use default DB
 	})
